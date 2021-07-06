a04c9387c04da927cdb4a2935a2bf6bc257ae11b
KevinGuzmanH <82849364+KevinGuzmanH@users.noreply.github.com>
2021-07-06 18:30:09 -0500

    make it better

---
+++ src/app/component/home/home.component.ts
@@ -0,0 +1,18 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import { PLATFORM_ID} from "@angular/core";
+import { isPlatformBrowser, isPlatformServer} from "@angular/common";
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(@Inject(PLATFORM_ID) private platformId: object) { }
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)){}
+  }
+
+}
